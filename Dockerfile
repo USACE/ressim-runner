9f33c055eb75c29f7aaccc0ce7bfa3ffc729900f
Will Lehman <william.p.lehman@usace.army.mil>
2023-09-17 21:37:53 +0000

    trying to update my code for the most up to date sdk and ressim linux distro

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,12 @@ FROM ubuntu:20.04 as dev
 #need to get the jdk.
 RUN apt update
 RUN apt upgrade -y
-RUN apt-cache search openjdk-11
-RUN apt -y install openjdk-11-jre
-RUN apt -y install openjdk-11-jdk
-
-#RUN apt -y install wget
-#RUN wget https://www.hec.usace.army.mil/nexus/repository/ressim-releases/mil/army/usace/hec/mil/army/usace/hec-ressim/hec-ressim-3.5.0.280-linux-x86_64.tar.gz -P /
-#RUN tar -xvzf /hec-ressim-3.5.0.280-linux-x86_64.tar.gz -C /
+RUN apt-cache search openjdk-8
+RUN apt -y install openjdk-8-jre
+RUN apt -y install openjdk-8-jdk
+#replace this with a dynamic call to a repo ultimately
+COPY cloud-compute/jar/cc-java-sdk-0.0.51-JR8.jar /cloud-compute/jar/cc-java-sdk-0.0.51-JR8.jar
+COPY rss/HEC-ResSim-3.5.0.280 /HEC-ResSim-3.5.0.280
 
 RUN apt -y install git
 RUN apt -y install libgfortran5
--- src/main/java/usace/wat/plugin/ressimrunner/ressimrunner.java
+++ src/main/java/usace/wat/plugin/ressimrunner/ressimrunner.java
@@ -18,11 +18,8 @@ import hec.rmi.csinterface.RmiWorkspace;
 import hec.rss.server.RssRMIServer;
 import hec.server.RmiAppImpl;
 import rma.util.RMAIO;
-
-import usace.wat.plugin.Message;
-import usace.wat.plugin.ModelPayload;
-import usace.wat.plugin.ResourcedFileData;
-import usace.wat.plugin.Utilities;
+import usace.cc.plugin.Payload;
+import usace.cc.plugin.PluginManager;
 
 
 public class ressimrunner  {
@@ -32,65 +29,10 @@ public class ressimrunner  {
      */
     public static void main(String[] args) {
         System.out.println(PluginName + " says hello.");
-        //check the args are greater than 1
-        
+        String wkspFile = "/HEC-ResSim-3.5.0.280/Examples/ExampleWatersheds/base/BaldEagle_V3.1/BaldEagle_V3.1.wksp";//modelOutputDestination;
+        String simFilePath = "/HEC-ResSim-3.5.0.280/Examples/ExampleWatersheds/base/BaldEagle_V3.1/rss/1993.11.27-1400.simperiod";//modelOutputDestination;
 
-        if(args.length!=2){
-            for(String s : args){
-                System.out.println("arg " + s);
-            }
-            System.out.println("Did not detect only payload `pathtopayload` argument");
-            return;
-        }else{
-            for(String s : args){
-                System.out.println("arg " + s);
-            }
-        }
-        //first arg should be a modelpayload check to see it is
-        String filepath = args[1];
-
-        /*Utilities.InitalizeFromEnv();
-        //load payload. 
-        ModelPayload mp = Utilities.LoadPayload(filepath);
-        */
-        ModelPayload mp = LoadLocalPayload(filepath);
-        String wkspName = "BaldEagle_V3.1";
-        String simName = RMAIO.userNameToFileName(mp.getModel().getName());
-        String altName = mp.getModel().getAlternative();
-        String altFileName = RMAIO.userNameToFileName(mp.getModel().getAlternative());
-
-        //copy the model to local if not localcompute
-        //hard coded outputdestination is fine in a container
-        //String modelOutputDestination = "/model/"+mp.getModel().getName()+"/";
-        System.setSecurityManager(new SecurityManager());
-
-        try {
-            RssRMIServer app = new RssRMIServer();
-        } catch (RemoteException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        boolean wkspFound = false;
-        String wkspFile = "/workspaces/ressim-runner/rss/hec-ressim-3.5.0.280-linux-x86_64.tar/ressim-280-wat/HEC-ResSim-3.5.0.280/Examples/ExampleWatersheds/base/BaldEagle_V3.1/BaldEagle_V3.1.wksp";//modelOutputDestination;
-        boolean altFound = false;
-        boolean simFound = false;
-        String simFilePath = "/workspaces/ressim-runner/rss/hec-ressim-3.5.0.280-linux-x86_64.tar/ressim-280-wat/HEC-ResSim-3.5.0.280/Examples/ExampleWatersheds/base/BaldEagle_V3.1/rss/1993.11.27-1400.simperiod";//modelOutputDestination;
-        //download the payload to list all input files
-        //Utilities.CopyPayloadInputsLocally(mp, modelOutputDestination);
-        for(ResourcedFileData input : mp.getInputs()){
-            if (input.getFileName().contains(wkspName + ".wksp")){
-                wkspFound = true;
-                wkspFile += input.getFileName();
-            }
-            if (input.getFileName().contains(altFileName +".malt")){
-                altFound = true;
-            }
-            if (input.getFileName().contains(simName + ".simperiod")){
-                simFound = true;
-                simFilePath += "rss/" + input.getFileName();
-            }
-        }
-        if (wkspFound && altFound && simFound){
+        if (true){
             RmiAppImpl rmiapp = RmiAppImpl.getApp();
             RmiFileManager filemanager = rmiapp.getFileManager();
             Identifier wkspid = new Identifier(wkspFile); 
@@ -100,8 +42,8 @@ public class ressimrunner  {
                 RmiWorkspace rssrmiwksp = rmiwksp.getChildWorkspace("rss");
                 Identifier simid = new Identifier(simFilePath);
                 SimulationPeriod sim = (SimulationPeriod)rssrmiwksp.getManager(SimulationPeriod.class.getName(),simid);
-                sim.loadWorkspace(null, "/workspaces/ressim-runner/rss/hec-ressim-3.5.0.280-linux-x86_64.tar/ressim-280-wat/HEC-ResSim-3.5.0.280/Examples/ExampleWatersheds/base/BaldEagle_V3.1/");//modelOutputDestination);
-                SimulationRun simrun = sim.getSimulationRun(altName);
+                sim.loadWorkspace(null, "/HEC-ResSim-3.5.0.280/Examples/ExampleWatersheds/base/BaldEagle_V3.1/");//modelOutputDestination);
+                SimulationRun simrun = sim.getSimulationRun("NoDSOps");
                 sim.setComputeAll(true);
                 sim.computeRun(simrun, -1);
             } catch (RemoteException e) {
@@ -128,19 +70,4 @@ public class ressimrunner  {
         }
         */
     }
-    private static ModelPayload LoadLocalPayload(String filepath) {
-        File file = new File(filepath);
-        final ObjectMapper mapper = new ObjectMapper(new YAMLFactory()); // jackson databind
-        try {
-            return mapper.readValue(file, ModelPayload.class);
-        } catch (Exception e) {
-            Message message = Message.BuildMessage()
-            .withMessage("Error Parsing Payload Contents: " + e.getMessage())
-            .fromSender("Plugin Services")
-            .build();
-            Utilities.Log(message);
-        }
-        return new ModelPayload();
-    }
-
 }
